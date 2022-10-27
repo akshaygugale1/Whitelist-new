fe76dcc1232e356a8653427832f79cc4bbeb824b
akshaygugale1 <akshaygugale1@gmail.com>
2022-10-27 22:44:24 +0530

    new changes

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -99,7 +99,7 @@ export default function Home() {
         throw new Error("Please cponnect to goerli");
       }
       if (needSigner) {
-        const signer = await web3Provider.getSigner();
+        const signer = web3Provider.getSigner();
         return signer;
       }
       return web3Provider;
@@ -120,11 +120,14 @@ export default function Home() {
   };
 
   useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
     if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
         network: "goerli",
         providerOptions: {},
-        disabledInjectedProvider: false,
+        disableInjectedProvider: false,
       });
       connectWallet();
     }
@@ -135,7 +138,7 @@ export default function Home() {
       <Head>
         <title>Whitelist Dapp</title>
         <meta name="description" content="Whitelist-Dapp" />
-        <link rel="icon" href="/favcon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.main}>
         <div>
